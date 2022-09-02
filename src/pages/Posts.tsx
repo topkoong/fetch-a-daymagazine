37575b3d765e7c0663783c03236e9ec3ee7f7bea
toppy <theerut@shoplive.dev>
2022-09-03 03:19:11 +0700

    Fixed bg color in post page

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -41,7 +41,7 @@ function Posts() {
     );
 
   return (
-    <article className='bg-bright-blue w-full h-full py-8'>
+    <article className='bg-bright-green w-full h-full py-8'>
       <PageHeader title={category} />
       <PageBreak />
       {isLoading ? (
