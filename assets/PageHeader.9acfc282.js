f420bc5d62c34404c84434b48aae71519aeffd9a
topkoong <topkoong@users.noreply.github.com>
2022-09-10 15:51:06 +0000

    deploy: a3dece504e8392fb9ee62590258be89c0be6837f

---
+++ assets/PageHeader.9acfc282.js
@@ -0,0 +1,1 @@
+import{e as a}from"./index.9ab13038.js";function t({title:e}){return a("h1",{className:"page-title",children:e})}export{t as default};
--- assets/PageHeader.f966f29c.js
+++
@@ -1,1 +0,0 @@
-import{e as a}from"./index.54fd8d9f.js";function t({title:e}){return a("h1",{className:"page-title",children:e})}export{t as default};
